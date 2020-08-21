b4be5013beb85e99bcda4e48bc10a8207a258451
Lee Stetson <stetsonthree@icloud.com>
2020-08-20 23:06:44 -0400

    Enables redirection
    - Enables redirection via show page
    - Introduces issue where other routes are intercepted
    - None the less, a functional product

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,9 @@
 class LinksController < ApplicationController
+  def show
+    link = Link.find_by(lookup_code: params[:lookup_code])
+    redirect_to link.original_url
+  end
+
   def create
     shortener = Shortener.new(link_params[:original_url])
     @link = shortener.generate_short_link
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   post "/links" => 'links#create'
+  get "/:lookup_code" => 'links#show'
 end
